85245505b7cf02f8aef41ac5cefe1e57f8db40d5
fatimaZahraMotaouakil <motaouakil.fatimazahra@gmail.com>
2023-06-06 16:30:03 +0100

    change style login nav register sidebar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
         $table->id();
-        $table->string('username');
+        $table->string('name');
         $table->string('email');
-        $table->string('mot_de_passe');
+        $table->string('password');
         $table->timestamps();
         });
     }
