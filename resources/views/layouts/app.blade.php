09be901ae1e3dd76304cd698262d8cff8b397e5e
fatimaZahraMotaouakil <motaouakil.fatimazahra@gmail.com>
2023-06-06 12:45:30 +0100

    Supprimer Non nessesaire fichier

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
+        <link rel="stylesheet" href="{{ asset("assets/style.css") }}">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     
@@ -107,6 +108,7 @@
             @yield('content')
         </main>
     </div>
+            <script  src="{{ asset("assets/style.js") }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
