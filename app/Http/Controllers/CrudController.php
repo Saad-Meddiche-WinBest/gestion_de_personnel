585b142d01847f79227c77d8f54bab7cd40c5292
saad <saadmeddiche2004201@gmail.com>
2023-06-07 14:31:00 +0100

    Creare helper file + fetch the columns and their type of a table

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// use Doctrine\DBAL\Schema\AbstractSchemaManager;
+use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 
 class CrudController extends Controller
 {
@@ -17,16 +21,67 @@ class CrudController extends Controller
         $New_Class = 'App\\Models\\' . ucfirst($name_of_table);
         $data = $New_Class::all();
 
-        // return view($name_of_table . 's.index', compact('data'));
-
         return view('display', compact(['data', 'name_of_table']));
     }
 
 
-    public function create()
+    // public function create(Request $request)
+    // {
+    //     $name_of_table = $request->tablename;
+
+    //     $columns = Schema::getColumnListing($name_of_table . 's');
+    //     $columns = array_diff($columns, ['id', 'created_at', 'updated_at']);
+
+    //     return view('create', compact(['columns', 'name_of_table']));
+    // }
+    public function create(Request $request)
     {
+        $name_of_table = $request->tablename . 's';
+
+        $columns = Schema::getConnection()->getDoctrineSchemaManager()->listTableColumns($name_of_table);
+        unset($columns['id']);
+        unset($columns['created_at']);
+        unset($columns['updated_at']);
+
+        $foreignKeys = DB::select(DB::raw("SELECT 
+        COLUMN_NAME, 
+        CONSTRAINT_NAME, 
+        REFERENCED_TABLE_NAME, 
+        REFERENCED_COLUMN_NAME
+        FROM 
+        information_schema.key_column_usage
+        WHERE 
+        table_schema = DATABASE()
+        AND table_name = '$name_of_table'"));
+
+        $columnData = [];
+
+        foreach ($columns as $column) {
+            $columnData[] = [
+                'name' => $column->getName(),
+                'type' => $column->getType()->getName(),
+                'default' => $column->getDefault(),
+                'foreign_key' => $this->getForeignKeyDetails($column->getName(), $foreignKeys),
+            ];
+        }
+
+        return view('create', compact(['columnData', 'name_of_table']));
     }
 
+    private function getForeignKeyDetails($columnName, $foreignKeys)
+    {
+        foreach ($foreignKeys as $foreignKey) {
+            if ($foreignKey->COLUMN_NAME === $columnName) {
+                return [
+                    'constraint_name' => $foreignKey->CONSTRAINT_NAME,
+                    'referenced_table' => $foreignKey->REFERENCED_TABLE_NAME,
+                    'referenced_column' => $foreignKey->REFERENCED_COLUMN_NAME,
+                ];
+            }
+        }
+
+        return null;
+    }
 
     public function store(Request $request)
     {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <div style="height:fit-content">
-        <p>qsdqsdqsdqsdqsdqsdqsdqsdqsdqsdqsdqs</p>
+        <form action="{{ route('Gerer.create') }}" method="GET">
+            <input type="hidden" name="tablename" value="service">
+            <button>Services</button>
+        </form>
+        <form action="{{ route('Gerer.create') }}" method="GET">
+            <input type="hidden" name="tablename" value="personne">
+            <button>Personnes</button>
+        </form>
     </div>
 @endsection
