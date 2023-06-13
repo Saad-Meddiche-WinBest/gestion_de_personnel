94ea4d45455eeab3934bed7573a0abdb4367ccfc
saad <saadmeddiche2004201@gmail.com>
2023-06-13 10:48:26 +0100

    Creation Of Migrations (nessecery tables for schedual absence calander).

---
+++ database/migrations/2023_06_13_094147_create_scheduale_absence_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absences', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_event');
+            $table->date('date');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_event')->references('id')->on('events');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scheduale_absence');
+    }
+};
