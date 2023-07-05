64fb95b83cabbf4bfc922b5bd99b070ce433ae39
S-A-3-D-O-U-N-E <112890463+saadMeddiche@users.noreply.github.com>
2023-07-05 12:16:33 +0100

    Remove The Button Add User

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,20 +21,28 @@
 
 
     @if ($name_of_model != 'absence' && $name_of_model != 'role')
-        <form action="{{ route('Gerer.create') }}" method="GET" style="width:100%;">
-            <input type="hidden" name="name_of_model" value="{{ $name_of_model }}">
-            <div style="width:100%;">
-                <button type="submit" class="btn btn-primary"
-                    style="float:right;  margin-top:15px;  margin-bottom:15px; font-size:1.5vh; ">
+        @if ($name_of_model != 'user')
+            <form action="{{ route('Gerer.create') }}" method="GET" style="width:100%;">
+                <input type="hidden" name="name_of_model" value="{{ $name_of_model }}">
+                <div style="width:100%;">
+                    <button type="submit" class="btn btn-primary"
+                        style="float:right;  margin-top:15px;  margin-bottom:15px; font-size:1.5vh; ">
 
-                    <svg xmlns="http://www.w3.org/2000/svg" style="margin-right:5px; margin-bottom: 2.5px;" width="16"
-                        height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                        <path
-                            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
-                    </svg>Add {{ ucfirst($name_of_model) }}</button>
+                        <svg xmlns="http://www.w3.org/2000/svg" style="margin-right:5px; margin-bottom: 2.5px;"
+                            width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+                        </svg>Add {{ ucfirst($name_of_model) }}</button>
+                </div>
+                <h2 style="font-family: 'Noto Sans TC', sans-serif;  margin:5px">Liste {{ ucfirst($name_of_model) }} </h2>
+            </form>
+        @else
+            <div style="width:100%">
+                <h2 style="font-family: 'Noto Sans TC', sans-serif;  margin:5px ; float:left">Liste
+                    {{ ucfirst($name_of_model) }} </h2>
             </div>
-            <h2 style="font-family: 'Noto Sans TC', sans-serif;  margin:5px">Liste {{ ucfirst($name_of_model) }} </h2>
-        </form>
+        @endif
     @endif
 
     @if ($name_of_model == 'absence')
@@ -57,6 +65,8 @@
             </div>
         </form>
     @endif
+
+
 
     <table id="myTable" class="table table-hover" style="background-color:white;">
         <button id="exportButton" class="btn btn-success" style="margin-top:15px; margin-bottom:15px;">Export to
@@ -90,7 +100,7 @@
                     <td>
                         <div style="display:flex; ">
 
-                            @if ($name_of_model !== 'role' )
+                            @if ($name_of_model !== 'role')
                                 <form action="{{ route('Gerer.edit', $data->id) }}" method="GET">
                                     <button id="btn1" type="sumbit" class="btn btn-primary"><svg
                                             xmlns="http://www.w3.org/2000/svg" width="16" height="16"
